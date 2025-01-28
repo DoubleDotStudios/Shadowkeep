4778051caa3a5333717e9a41b45905ff837de85a
DoubleDotStudios <doubledotgames@gmail.com>
2025-01-29 02:51:36 +1300

    Worked on the wizard boss.

--- Entities/Wizard.gd
+++ Entities/Wizard.gd
@@ -6,40 +6,47 @@ var inside: bool = false
 
 func _ready() -> void:
 	nav.target_position = adventurer.global_position
-	self.state = State.Chase
 
-#func _physics_process(delta: float) -> void:
-	#if self.state == State.Defeat:
-		#queue_free()
-	#
-	#if self.state == State.Chase:
-		#coll.set_deferred("disabled", false)
-		#
-		#var currPos: Vector2 = global_position
-		#var nextPos: Vector2 = nav.get_next_path_position()
-		#
-		#nav.target_position = adventurer.global_position
-		#
-		#velocity = currPos.direction_to(nextPos) * speed * delta
-		#
-		#move_and_slide()
-	#elif self.state == State.Wait:
-		#coll.set_deferred("disabled", true)
-		#nav.target_position = Vector2.ZERO
-	#elif self.state == State.Hit:
-		#if nav.target_reached:
-			#self.state = State.Chase
-		#
-		#coll.set_deferred("disabled", true)
-		#$DamageComponent/AreaShape.set_deferred("disabled", true)
-		#runAway()
-		#
-		#var currPos: Vector2 = global_position
-		#var nextPos: Vector2 = nav.get_next_path_position()
-		#
-		#velocity = currPos.direction_to(nextPos) * speed * delta
-		#
-		#move_and_slide()
+func _physics_process(delta: float) -> void:
+	if self.state == State.Prepare:
+		var t = Timer.new()
+		add_child(t)
+		t.start(0.1)
+		await t.timeout
+		t.queue_free()
+		self.state = State.Chase
+	
+	if self.state == State.Defeat:
+		queue_free()
+	
+	elif self.state == State.Wait:
+		coll.set_deferred("disabled", true)
+		nav.target_position = Vector2.ZERO
+	elif self.state == State.Hit:
+		if nav.target_reached:
+			self.state = State.Chase
+		
+		coll.set_deferred("disabled", true)
+		$DamageComponent/AreaShape.set_deferred("disabled", true)
+		runAway()
+		
+		var currPos: Vector2 = global_position
+		var nextPos: Vector2 = nav.get_next_path_position()
+		
+		velocity = currPos.direction_to(nextPos) * speed * delta
+		
+		move_and_slide()
+	elif self.state == State.Chase:
+		coll.set_deferred("disabled", false)
+		
+		var currPos: Vector2 = global_position
+		var nextPos: Vector2 = nav.get_next_path_position()
+		
+		nav.target_position = adventurer.global_position
+		
+		velocity = currPos.direction_to(nextPos) * speed * delta
+		
+		move_and_slide()
 
 func _on_damage_component_attacked() -> void:
 	self.state = State.Hit
